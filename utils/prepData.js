d280c5d1af4f45f079036af8303a5a93f29dd4ce
SergiiMost <s.v.mostovoyi@gmail.com>
2020-08-16 12:08:14 -0400

    daily update

--- utils/prepData.js
+++ utils/prepData.js
@@ -1,19 +1,8 @@
 export const prepData = [
   {
-    date: "2020-01-08",
+    date: "2020-08-16",
     tasks: {
-      1: 'Finished 3 videos from Udemy course <a href="https://www.udemy.com/" target="_blank">Udemy</a>',
-      2: "sdfdsf gfsdfas",
-      3: "sadfsdfasdfsdf",
-    },
-  },
-  {
-    date: "2020-01-09",
-    tasks: {
-      1: "fasdfsdaf",
-      2: "sdfdsf gfsdfas",
-      3: "sadfsdfasdfsdf",
-      4: "sadfsdfasdfsdf",
+      1: "Deployed this website to Vercel 🎉",
     },
   },
 ]

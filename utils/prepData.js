813886568abd9361f7afbdf6581c5b56a43e644c
SergiiMost <s.v.mostovoyi@gmail.com>
2020-08-18 22:45:40 -0400

    daily update

--- utils/prepData.js
+++ utils/prepData.js
@@ -1,5 +1,13 @@
 export const prepData = [
   {
+    date: "2020-08-18",
+    tasks: {
+      1: "Finished <b>lectures 67-73</b> of JavaScript Algorithms and Data Structures Masterclass (<a href='https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/' target='_blank' rel='noreferrer'>Udemy</a>)",
+      2: "Solved Leetcode problem <b>#14</b> (<a href='https://github.com/SergiiMost/LeetCode/blob/master/Easy_longest_common_prefix-14.js' target='_blank' rel='noreferrer'>GitHub</a>)",
+      3: "Reviewed  chapters <b>2.12-2.18</b> of <a href='https://javascript.info/' target='_blank' rel='noreferrer'>javascript.info</a>",
+    },
+  },
+  {
     date: "2020-08-17",
     tasks: {
       1: "Finished <b>lectures 55-65</b> of JavaScript Algorithms and Data Structures Masterclass (<a href='https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/' target='_blank' rel='noreferrer'>Udemy</a>)",

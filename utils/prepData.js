4fb874396321934be98bd3c0a9fcd20c34dcb56c
SergiiMost <s.v.mostovoyi@gmail.com>
2020-08-22 21:43:54 -0400

    daily update

--- utils/prepData.js
+++ utils/prepData.js
@@ -1,5 +1,13 @@
 export const prepData = [
   {
+    date: "2020-08-22",
+    tasks: {
+      1: "Finished <b>lectures 109-126</b> of JavaScript Algorithms and Data Structures Masterclass (<a href='https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/' target='_blank' rel='noreferrer'>Udemy</a>)",
+      2: "Solved Leetcode problem <b>#83</b> (<a href='https://github.com/SergiiMost/LeetCode/blob/master/Easy_remove_duplicates_sorted_linked_list-83.js' target='_blank' rel='noreferrer'>GitHub</a>)",
+      3: "Reviewed  chapters <b>5.6-5.12</b> of <a href='https://javascript.info/' target='_blank' rel='noreferrer'>javascript.info</a>",
+    },
+  },
+  {
     date: "2020-08-20",
     tasks: {
       1: "Finished <b>lectures 104-108</b> of JavaScript Algorithms and Data Structures Masterclass (<a href='https://www.udemy.com/course/js-algorithms-and-data-structures-masterclass/' target='_blank' rel='noreferrer'>Udemy</a>)",

002f9dbe4ba1bd7b71f276ab516c49ce003f5842
SergiiMost <s.v.mostovoyi@gmail.com>
2020-09-10 20:50:43 -0400

    update

--- pages/blog.js
+++ pages/blog.js
@@ -28,11 +28,7 @@ export default function Blog() {
         <main className={styles.main}>
           <section>
             <Wrapper>
-              {/* <BlogCard
-                title='An interview  preparation and beyond. Chronological overview of my work. '
-                timeUpdated={prepData[0].date}
-                url='/posts/inretview-prep'
-              /> */}
+
               <ComingSoon />
             </Wrapper>
           </section>
